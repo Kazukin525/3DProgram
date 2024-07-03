133726208185ec72f6d7a957202a1a203d265e58
Kazukin525 <165025229+Kazukin525@users.noreply.github.com>
2024-07-03 11:14:25 +0900

    新環境、監視カメラ作成中

---
+++ L05_Camera03/Src/Application/GameObject/Camera/CCTVCamera/CCTVCamera.cpp
@@ -0,0 +1,15 @@
+﻿#include "CCTVCamera.h"
+
+void CCTVCamera::Init()
+{
+	// 親クラスの初期化呼び出し
+	CameraBase::Init();
+
+	// 注視点
+	m_mLocalPos = Math::Matrix::CreateTranslation(0, 1.5f, -10.0f);
+}
+
+void CCTVCamera::PostUpdate()
+{
+	m_mWorld = m_mLocalPos;
+}
