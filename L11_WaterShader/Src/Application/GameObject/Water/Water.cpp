20649144c19d16902435dcaa32bf7aec3a6dee71
Kazukin525 <165025229+Kazukin525@users.noreply.github.com>
2024-10-21 11:29:49 +0900

    水の表現、ノーマルマップ

---
+++ L11_WaterShader/Src/Application/GameObject/Water/Water.cpp
@@ -0,0 +1,35 @@
+﻿#include "Water.h"
+
+void Water::DrawLesson()
+{
+	if (!m_spModel) { return; }
+	
+	KdShaderManager::Instance().m_LessonShader.SetUVOffset(m_UVOffset);	
+	KdShaderManager::Instance().m_LessonShader.DrawModel(*m_spModel, m_mWorld);
+}
+
+void Water::Update()
+{
+	m_UVOffset.x += 0.0001f;
+	m_UVOffset.y += 0.0001f;
+
+	if (m_UVOffset.x > 1.0f)
+	{
+		m_UVOffset.x -= 1.0f;
+		m_UVOffset.y -= 1.0f;
+	}
+}
+
+void Water::Init()
+{
+	if (!m_spModel)
+	{
+		m_spModel = std::make_shared<KdModelWork>();
+		m_spModel->SetModelData("Asset/LessonData/Stage/Water/Water.gltf");
+	}
+
+	Math::Matrix _transMat = Math::Matrix::CreateTranslation(Math::Vector3(0.0f, 0.3f, 0.0f));
+	Math::Matrix _scaleaMat = Math::Matrix::CreateScale(5.0f);
+
+	m_mWorld = _scaleaMat * _transMat;
+}
---
+++ L11_WaterShader/Src/Application/GameObject/Water/Water.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+class Water : public KdGameObject
+{
+public:
+	Water()				{}
+	~Water()override	{}
+
+	void DrawLesson()override;
+	void Update()override;
+	void Init()		override;
+
+private:
+
+	Math::Vector2					m_UVOffset;
+
+	std::shared_ptr<KdModelWork>	m_spModel;
+};
